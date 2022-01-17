d560b51ffbf8d9036ce695d4ddcf07b3fe96adcb
markwithers <mark26266@gmail.com>
2022-01-17 19:40:20 +0000

    Add prettier

--- src/1.ts
+++ src/1.ts
@@ -1,31 +1,31 @@
-import * as RA from 'fp-ts/ReadonlyArray';
-import * as T from 'fp-ts/Tuple';
-import {transpose, sum} from 'fp-ts-std/ReadonlyArray';
-import {lines} from 'fp-ts-std/String';
-import {pipe} from 'fp-ts/lib/function';
-import fs from 'fs';
+import * as RA from 'fp-ts/ReadonlyArray'
+import * as T from 'fp-ts/Tuple'
+import { transpose, sum } from 'fp-ts-std/ReadonlyArray'
+import { lines } from 'fp-ts-std/String'
+import { pipe } from 'fp-ts/lib/function'
+import fs from 'fs'
 
 pipe(
   fs.readFileSync('./data/1.txt', 'utf-8'),
   lines,
-  RA.map(string => parseInt(string, 10)),
+  RA.map((string) => parseInt(string, 10)),
   RA.reduce<number, [number, number]>([0, Infinity], ([inc, last], num) =>
-    num > last ? [inc + 1, num] : [inc, num],
+    num > last ? [inc + 1, num] : [inc, num]
   ),
   T.fst,
-  console.log,
-);
+  console.log
+)
 
 pipe(
   fs.readFileSync('./data/1.txt', 'utf-8'),
   lines,
-  RA.map(string => parseInt(string, 10)),
-  numbers =>
+  RA.map((string) => parseInt(string, 10)),
+  (numbers) =>
     transpose([numbers, RA.dropLeft(1)(numbers), RA.dropLeft(2)(numbers)]),
   RA.map(sum),
   RA.reduce<number, [number, number]>([0, Infinity], ([inc, last], num) =>
-    num > last ? [inc + 1, num] : [inc, num],
+    num > last ? [inc + 1, num] : [inc, num]
   ),
   T.fst,
-  console.log,
-);
+  console.log
+)
